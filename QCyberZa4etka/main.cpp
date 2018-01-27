8c5d6bb3d0682b1fb3b2916078bd43d165b03ff3
baz2142 <www.baz.ru@gmail.com>
2018-01-27 17:22:27 +0500

    Initial main window makeup

--- QCyberZa4etka/main.cpp
+++ QCyberZa4etka/main.cpp
@@ -3,18 +3,24 @@
 
 #include "StudentView.hpp"
 
+#include "MainAppWindow.hpp"
+
 int main(int argc, char *argv[])
 {
-    srandom(time(0));
+//    srandom(time(0));
 
     QApplication a(argc, argv);
-    MainWindow w;
-    w.resize(800, 600);
-    w.show();
+//    MainWindow w;
+//    w.resize(800, 600);
+//    w.show();
 
 //    Student stud = Student::getRandom();
 //    StudentPresentation w;
 //    w.setEntity(&stud);
+
+    MainAppWindow w;
+    w.resize(1280, 720);
+    w.show();
 
 
     return a.exec();
